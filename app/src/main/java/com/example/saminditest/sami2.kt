4a3ff17fcc37e6f4445cab2564b21a33957c68c5
sahas <sahasra.weerasinghe99@gmail.com>
2023-06-08 00:59:41 +0530

    sami 2

---
+++ app/src/main/java/com/example/saminditest/sami2.kt
@@ -0,0 +1,11 @@
+package com.example.saminditest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class sami2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sami2)
+    }
+}
